8a03b655597ff8d23fcad3f3fa1ed2b30d82f7b4
Miu <miyuuu0728@gmail.com>
2021-05-19 18:44:57 -0700

    added top btn

--- src/components/05-Footer/TopArrow.jsx
+++ src/components/05-Footer/TopArrow.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import Arrow from "../../images/topArrow.png";
 
 const TopArrow = () => {
+  const handleTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <div className="absolute right-5 top-100" data-aos="fade-right">
+    <div
+      className="absolute right-5 top-100"
+      onClick={handleTop}
+      data-aos="fade-right"
+    >
       <a href="#header">
         <img src={Arrow} alt="top" className="top_arrow" />
         <p className="transform rotate-90 text-bl tracking-widest mt-6">TOP</p>
